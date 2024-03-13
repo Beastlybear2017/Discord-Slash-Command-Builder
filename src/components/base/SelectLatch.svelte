9222e2c3e7d78a40749fdf23718eb2490471474c
Kieran B <kieran007090@gmail.com>
2024-03-13 01:31:31 +0000

    Fix?

--- src/components/base/SelectLatch.svelte
+++ src/components/base/SelectLatch.svelte
@@ -97,7 +97,7 @@
         {#if opened}
             <div class="select-options" bind:this={optionsElements}>
                 {#each options as option, i}
-                    {#await selected.includes(option) ? selected1 = true : selected1 = false, console.log(selected1)}
+                    {#await selected.includes(option) ? selected1 = true : selected1 = false}
                     <d></d>
                     {/await}
                     <div class="option-{selected1}" on:mouseup={() => handleSelection(i)}>
